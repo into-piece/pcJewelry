95b2d299a2cedf317a5be9e0e59dc349cb40aeb0
intopiece <if-ok@163.com>
2019-09-02 01:02:22 +0800

    feat: 换背景图

--- src/pages/User/Login.js
+++ src/pages/User/Login.js
@@ -55,7 +55,7 @@ class LoginPage extends Component {
         },
       });
       this.setState({
-        isFrist:false,
+        isFrist: false,
       })
     }
   };
@@ -72,7 +72,7 @@ class LoginPage extends Component {
 
   render() {
     const { login, submitting } = this.props;
-    const { type, autoLogin,isFrist=true } = this.state;
+    const { type, autoLogin, isFrist = true } = this.state;
 
     // console.log(" logi response  ",login)
 
@@ -88,78 +88,78 @@ class LoginPage extends Component {
         >
 
           {/*<Tab key="account" tab={formatMessage({ id: 'app.login.tab-login-credentials' })}>*/}
-            {login &&
-             login.rtnCode !== '000000' &&
-             !isFrist &&
-              !submitting &&
-              // this.renderMessage(formatMessage({ id: 'app.login.message-invalid-credentials' }))}
-              this.renderMessage(login.rtnMsg)}
-            <UserName
-              name="userName"
-              placeholder={`${formatMessage({ id: 'app.login.userName' })}`}
-              rules={[
-                {
-                  required: true,
-                  message: formatMessage({ id: 'validation.userName.required' }),
-                },
-              ]}
-            />
-            <Password
-              name="password"
-              placeholder={`${formatMessage({ id: 'app.login.password' })}`}
-              rules={[
-                {
-                  required: true,
-                  message: formatMessage({ id: 'validation.password.required' }),
-                },
-              ]}
-              onPressEnter={e => {
-                e.preventDefault();
-                this.loginForm.validateFields(this.handleSubmit);
-              }}
-            />
+          {login &&
+            login.rtnCode !== '000000' &&
+            !isFrist &&
+            !submitting &&
+            // this.renderMessage(formatMessage({ id: 'app.login.message-invalid-credentials' }))}
+            this.renderMessage(login.rtnMsg)}
+          <UserName
+            name="userName"
+            placeholder={`${formatMessage({ id: 'app.login.userName' })}`}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'validation.userName.required' }),
+              },
+            ]}
+          />
+          <Password
+            name="password"
+            placeholder={`${formatMessage({ id: 'app.login.password' })}`}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'validation.password.required' }),
+              },
+            ]}
+            onPressEnter={e => {
+              e.preventDefault();
+              this.loginForm.validateFields(this.handleSubmit);
+            }}
+          />
           {/*</Tab>*/}
           {/*<Tab key="mobile" tab={formatMessage({ id: 'app.login.tab-login-mobile' })}>*/}
-            {/*{login.status === 'error' &&*/}
-              {/*login.type === 'mobile' &&*/}
-              {/*!submitting &&*/}
-              {/*this.renderMessage(*/}
-                {/*formatMessage({ id: 'app.login.message-invalid-verification-code' })*/}
-              {/*)}*/}
-            {/*<Mobile*/}
-              {/*name="mobile"*/}
-              {/*placeholder={formatMessage({ id: 'form.phone-number.placeholder' })}*/}
-              {/*rules={[*/}
-                {/*{*/}
-                  {/*required: true,*/}
-                  {/*message: formatMessage({ id: 'validation.phone-number.required' }),*/}
-                {/*},*/}
-                {/*{*/}
-                  {/*pattern: /^1\d{10}$/,*/}
-                  {/*message: formatMessage({ id: 'validation.phone-number.wrong-format' }),*/}
-                {/*},*/}
-              {/*]}*/}
-            {/*/>*/}
-            {/*<Captcha*/}
-              {/*name="captcha"*/}
-              {/*placeholder={formatMessage({ id: 'form.verification-code.placeholder' })}*/}
-              {/*countDown={120}*/}
-              {/*onGetCaptcha={this.onGetCaptcha}*/}
-              {/*getCaptchaButtonText={formatMessage({ id: 'form.get-captcha' })}*/}
-              {/*getCaptchaSecondText={formatMessage({ id: 'form.captcha.second' })}*/}
-              {/*rules={[*/}
-                {/*{*/}
-                  {/*required: true,*/}
-                  {/*message: formatMessage({ id: 'validation.verification-code.required' }),*/}
-                {/*},*/}
-              {/*]}*/}
-            {/*/>*/}
+          {/*{login.status === 'error' &&*/}
+          {/*login.type === 'mobile' &&*/}
+          {/*!submitting &&*/}
+          {/*this.renderMessage(*/}
+          {/*formatMessage({ id: 'app.login.message-invalid-verification-code' })*/}
+          {/*)}*/}
+          {/*<Mobile*/}
+          {/*name="mobile"*/}
+          {/*placeholder={formatMessage({ id: 'form.phone-number.placeholder' })}*/}
+          {/*rules={[*/}
+          {/*{*/}
+          {/*required: true,*/}
+          {/*message: formatMessage({ id: 'validation.phone-number.required' }),*/}
+          {/*},*/}
+          {/*{*/}
+          {/*pattern: /^1\d{10}$/,*/}
+          {/*message: formatMessage({ id: 'validation.phone-number.wrong-format' }),*/}
+          {/*},*/}
+          {/*]}*/}
+          {/*/>*/}
+          {/*<Captcha*/}
+          {/*name="captcha"*/}
+          {/*placeholder={formatMessage({ id: 'form.verification-code.placeholder' })}*/}
+          {/*countDown={120}*/}
+          {/*onGetCaptcha={this.onGetCaptcha}*/}
+          {/*getCaptchaButtonText={formatMessage({ id: 'form.get-captcha' })}*/}
+          {/*getCaptchaSecondText={formatMessage({ id: 'form.captcha.second' })}*/}
+          {/*rules={[*/}
+          {/*{*/}
+          {/*required: true,*/}
+          {/*message: formatMessage({ id: 'validation.verification-code.required' }),*/}
+          {/*},*/}
+          {/*]}*/}
+          {/*/>*/}
           {/*</Tab>*/}
           <div>
-            <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
+            <Checkbox checked={autoLogin} onChange={this.changeAutoLogin} style={{ color: '#fff' }}>
               <FormattedMessage id="app.login.remember-me" />
             </Checkbox>
-            <a style={{ float: 'right' }} href="">
+            <a style={{ float: 'right', color: '#fff' }} href="">
               <FormattedMessage id="app.login.forgot-password" />
             </a>
           </div>
@@ -167,13 +167,13 @@ class LoginPage extends Component {
             <FormattedMessage id="app.login.login" />
           </Submit>
           {/*<div className={styles.other}>*/}
-            {/*<FormattedMessage id="app.login.sign-in-with" />*/}
-            {/*<Icon type="alipay-circle" className={styles.icon} theme="outlined" />*/}
-            {/*<Icon type="taobao-circle" className={styles.icon} theme="outlined" />*/}
-            {/*<Icon type="weibo-circle" className={styles.icon} theme="outlined" />*/}
-            {/*<Link className={styles.register} to="/user/register">*/}
-              {/*<FormattedMessage id="app.login.signup" />*/}
-            {/*</Link>*/}
+          {/*<FormattedMessage id="app.login.sign-in-with" />*/}
+          {/*<Icon type="alipay-circle" className={styles.icon} theme="outlined" />*/}
+          {/*<Icon type="taobao-circle" className={styles.icon} theme="outlined" />*/}
+          {/*<Icon type="weibo-circle" className={styles.icon} theme="outlined" />*/}
+          {/*<Link className={styles.register} to="/user/register">*/}
+          {/*<FormattedMessage id="app.login.signup" />*/}
+          {/*</Link>*/}
           {/*</div>*/}
         </Login>
       </div>
