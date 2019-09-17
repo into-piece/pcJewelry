ad35315e4641790931de00997170a01cff5a3242
frank-zeng <328255220@qq.com>
2019-09-17 17:43:20 +0800

    ...

--- config/router.config.js
+++ config/router.config.js
@@ -55,7 +55,7 @@ export default [
     path: '/',
     component: '../layouts/BasicLayout',
     Routes: ['src/pages/Authorized'],
-    authority: ['admin', 'user', 'chents'],
+    // authority: ['admin', 'user', 'chents'],
     routes: [
       // dashboard
       {
