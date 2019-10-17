0944096713b53ea8920587247387abba6ab1c81e
frank-zeng <328255220@qq.com>
2019-10-17 10:49:20 +0800

    modal拖动

--- src/components/BuildTitle/index.js
+++ src/components/BuildTitle/index.js
@@ -3,14 +3,20 @@ import DragM from 'dragm';
 import 'antd/dist/antd.css';
 
 class BuildTitle extends React.Component {
+
   updateTransform = transformStr => {
-    this.modalDom.style.transform = transformStr;
+    // this.modalDom && this.modalDom.forEach(e => {
+    //   e.style.transform = transformStr;
+    // });
+    Array.prototype.forEach.call(this.modalDom, function(element) {
+      element.style.transform = transformStr;
+    });
   };
 
   componentDidMount() {
     this.modalDom = document.getElementsByClassName(
       'ant-modal-wrap',
-    )[0];
+    );
   }
 
   render() {
