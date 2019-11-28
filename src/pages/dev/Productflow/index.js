7b950c56f7982069262860110bd1a77fb0e133db
frank-zeng <328255220@qq.com>
2019-11-29 00:23:23 +0800

    视频上传组件 v1.0

--- src/pages/dev/Productflow/index.js
+++ src/pages/dev/Productflow/index.js
@@ -228,6 +228,7 @@ class Index extends Component {
       case 7:
         return (<Select
           placeholder="请选择"
+          mode="multiple"
           showSearch
           optionFilterProp="children"
           filterOption={(input, option) =>
@@ -349,6 +350,14 @@ class Index extends Component {
 
     const dataArr = modalInput[rightActive];
     const fieldslist = dataArr.map(e=>e.value)
+
+    const filelist = this.state.filelist.flatMap(e => e.url);
+    const videolist = this.state.videolist.flatMap(e => e.url);
+
+    if(['productProcess'].indexOf(rightActive)>-1){
+      params={...params,picPath:filelist,videoPath:videolist}
+    }
+
     form.validateFields(fieldslist,(err, values) => {
       if (!err) {
         params = {
@@ -371,6 +380,8 @@ class Index extends Component {
               this.getListSecond({ type: secondTableActive });
             }
             if (close) this.btnFn('');
+            if (close)  this.setState({ filelist:[],videolist: [] });
+
           }
         });
       }
@@ -481,7 +492,7 @@ class Index extends Component {
       case 'plus':
       case 'edit':
       default:
-        this.setState({ modalType });
+        this.setState({ modalType ,filelist:[],videolist: [] });
         break;
       case 'delete':
         ModalConfirm({
@@ -568,7 +579,6 @@ class Index extends Component {
     } = this;
     const { modalType, rightActive, secondTableActive, addloading } = state;
     const { choosenRowData, choosenRowDataSecond } = props;
-
     const modalFooter = modalType === 'plus' ? [
       <Button
         key="back"
