659cbacc6fbea73313e08f24238beb3878c231a6
frank-zeng <328255220@qq.com>
2019-12-07 11:53:41 +0800

    默认图片

--- src/pages/dev/Productflow/config/modalInput.js
+++ src/pages/dev/Productflow/config/modalInput.js
@@ -9,7 +9,7 @@ export const productflow = [
   { 'key': '计价类别', 'value': 'valuationClass' ,  'type': 6, 'arr': [{ 'key': '计重', 'value': 0 }, { 'key': '计件', 'value': 1 }], 'dfv': 0 },
   { 'key': '允许损耗(%)', 'value': 'isWastage' ,  'number': true,},
   { 'key': '出货重量范围', 'value': 'weightRange' ,  'number': true},
-  { 'key': '说明', 'value': 'remarks' },
+  { 'key': '说明', 'value': 'remarks', type:8 },
 
 ];
 
@@ -19,6 +19,7 @@ export const productProcess = [
   { 'key': '英文名称', 'value': 'engName', 'noNeed': true},
   { 'key': '损耗(%)', 'value': 'isWastage',   'number': true },
   { 'key': '产能(件/小时)', 'value': 'processCapacity', 'number': true,  },
+  { 'key': '备注', 'value': 'remarks', type:8  },
 ];
 
 
