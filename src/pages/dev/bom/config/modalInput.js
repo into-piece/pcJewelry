2c9b5428e5ace5be68007e3d9beea6679e460e2b
intopiece <if-ok@163.com>
2019-12-16 23:27:43 +0800

    feat: 新增原料信息弹窗

--- src/pages/dev/bom/config/modalInput.js
+++ src/pages/dev/bom/config/modalInput.js
@@ -1,25 +1,25 @@
 export const product = [{ key: 'BOM名称', value: 'bName', type: 1 }];
 
 export const material = [
-  { key: 'bom名称', value: 'productNo', type: 2, list: 'boomList' },
-  { key: '原料类别', value: 'filmCode', type: 2, list: 'listMstWordbook' },
-  { key: '模具编号', value: 'membraneProportion', type: 2, list: 'listFilmSettingsDropDown' },
-  { key: '模比', value: 'noteWaxImmediately' },
+  { key: 'bom名称', value: 'BomId', type: 2, list: 'boomList' },
+  { key: '原料类别', value: 'materialType', type: 2, list: 'listMstWordbook' },
+  { key: '模具编号', value: 'modelNo', type: 2, list: 'listFilmSettingsDropDown' },
+  { key: '模比', value: 'modulusRatio' },
   {
     key: '原料小类',
-    value: 'intoFilmPressure',
+    value: 'materialSub',
     type: 2,
     list: 'getTypeByWordbookCode',
   },
   {
     key: '领料部门',
-    value: 'squeezeFilmPressure',
+    value: 'acquisitionDepartment',
     type: 2,
     list: '',
   },
   {
     key: '配模',
-    value: 'waxInjectionPressure',
+    value: 'filmMatch',
     type: 6,
     arr: [
       { key: '是', value: 1 },
@@ -27,14 +27,18 @@ export const material = [
     ],
     initValue: 1,
   },
-  { key: '原料编号', value: 'vacuumTime', type: 2, list: '' },
-  {
-    key: '注蜡时间(秒)',
-    value: 'waxInjectionTime',
-  },
-  { key: '蜡件重量(克)', value: 'waxWeight', number: true, initValue: 0.0, step: 0.01, min: 0 },
-  { key: '存放位置', value: 'position', type: 2, list: 'listMoldPositioningSettingsDropDown' },
-  { key: '备注', value: 'remarks', type: 8, noNeed: true },
+  { key: '原料编号', value: 'materialNo', type: 2, list: '' },
+  { key: '单件用量', value: 'singleDosage' },
+  { key: '规格', value: 'specification' },
+  { key: '中文名', value: 'zhName' },
+  { key: '英文名', value: 'enName' },
+  { key: '重量单位', value: 'weightUnit' },
+  { key: '计量单位', value: 'measureUnit' },
+  { key: '单重', value: 'inventoryWeight' },
+  { key: '单件用量', value: 'singleDosage' },
+  { key: '计价类别', value: 'valuationClass' },
+  { key: '损耗率', value: 'attritionRate' },
+  { key: '备注', value: 'remarks' },
 ];
 
 export default {
