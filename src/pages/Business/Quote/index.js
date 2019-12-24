abdfbed6677ad95141a2c578ec251dd1b07e7f45
916797770@qq.com <916797770@qq.com>
2019-12-25 00:39:08 +0800

    修改展示页面的配置

--- src/pages/Business/Quote/index.js
+++ src/pages/Business/Quote/index.js
@@ -628,19 +628,6 @@ class Info extends Component {
             }}
           />
         );
-
-      case 11:
-        return (
-          <Radio.Group disabled style={{ width: '100' }}>
-            {arr.map(({ key, value }) => {
-              return (
-                <Radio value={value} key={value}>
-                  {key}
-                </Radio>
-              );
-            })}
-          </Radio.Group>
-        );
       default:
         return (
           <Input
@@ -1120,7 +1107,6 @@ class Info extends Component {
         res.body.records.length > 0
       ) {
         productLineCoefficientQuotation = res.body.records[0].productLineCoefficientQuotation;
-        console.log("call back  q ",res.body.records[0]);
       }
     });
     await geInitializeCountByProductId({
@@ -1303,8 +1289,8 @@ class Info extends Component {
             </Button>,
           ];
 
-    // console.log(choosenRowData, choosenRowData.id);
-    // console.log(productList, '=======');
+    console.log(choosenRowData, choosenRowData.id);
+    console.log(productList, '=======');
 
     return (
       <div className={styles.page}>
