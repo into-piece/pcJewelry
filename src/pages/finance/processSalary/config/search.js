ce72652c710168a552186af26e69aa740a613c4a
frank-zeng <328255220@qq.com>
2020-01-05 15:18:58 +0800

    财务-流程薪资

--- src/pages/finance/processSalary/config/search.js
+++ src/pages/finance/processSalary/config/search.js
@@ -1,10 +1,10 @@
 
 // 生产流程的筛选参数
 export const processSalary = [
-  { key: '编号', value: 'code' },
-  { key: '产品类别', value: 'productType', 'type': 2, 'list': 'H016003'},
-  { key: '工费类别', value: 'costType'},
   { key: '流程编号', value: 'flowCode', 'type': 2, 'list': 'listProductionFlowDropDown'},
+  { key: '流程名称', value: 'flowName'},
+  { key: '英文名称', value: 'enName'},
+  { key: '状态', value: 'status', 'type': 2, 'list': 'statusList',},
 
 ];
 
--- src/pages/finance/processSalary/models/model.js
+++ src/pages/finance/processSalary/models/model.js
@@ -34,7 +34,7 @@ export default {
 
     searchParams: {},
 
-
+    statusList: [{key:"不限",value:undefined},{key:"输入",value:0},{key:"已审批",value:2}],
     listProductionFlowDropDown: [{ key: '', value: '' }],
     H016003: [{ key: '', value: '' }],
 
